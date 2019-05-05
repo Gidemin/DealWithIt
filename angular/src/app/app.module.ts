f22abedab3480b64806552c5cf4f2656c4ee0c33
Gidemin <arabchik.viktor@mail.ru>
2019-05-05 15:37:31 +0300

    Sorted components in app module

--- angular/src/app/app.module.ts
+++ angular/src/app/app.module.ts
@@ -3,14 +3,14 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { TaskListComponent } from './task-list/task-list.component';
 import { CreateTaskComponent } from './create-task/create-task.component';
+import { TaskListComponent } from './task-list/task-list.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    TaskListComponent,
-    CreateTaskComponent
+    CreateTaskComponent,
+    TaskListComponent
   ],
   imports: [
     BrowserModule,
