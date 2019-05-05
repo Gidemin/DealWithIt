a1d34b549cc4371b205ad62b04fcf3b183140447
Gidemin <arabchik.viktor@mail.ru>
2019-05-05 15:24:30 +0300

    Added task-list and task-list component

--- angular/src/app/app.module.ts
+++ angular/src/app/app.module.ts
@@ -3,10 +3,14 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { TaskListComponent } from './task-list/task-list.component';
+import { CreateTaskComponent } from './create-task/create-task.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    TaskListComponent,
+    CreateTaskComponent
   ],
   imports: [
     BrowserModule,
