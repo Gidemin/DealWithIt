520280f99dbadeda6b9688af770876d847e35b97
Gidemin <arabchik.viktor@mail.ru>
2019-05-06 12:44:29 +0300

    Cleared default text. Added simple routes between task list create task components

--- angular/src/app/create-task/create-task.component.ts
+++ angular/src/app/create-task/create-task.component.ts
@@ -1,15 +1,17 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-create-task',
   templateUrl: './create-task.component.html',
   styleUrls: ['./create-task.component.scss']
 })
-export class CreateTaskComponent implements OnInit {
+export class CreateTaskComponent {
 
-  constructor() { }
+  constructor(private router: Router) { }
 
-  ngOnInit() {
+  backToTaskList() {
+    this.router.navigate(['/taskList']);
   }
 
 }
--- angular/src/app/task-list/task-list.component.ts
+++ angular/src/app/task-list/task-list.component.ts
@@ -1,15 +1,17 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
+import {  Router } from '@angular/router';
 
 @Component({
   selector: 'app-task-list',
   templateUrl: './task-list.component.html',
   styleUrls: ['./task-list.component.scss']
 })
-export class TaskListComponent implements OnInit {
+export class TaskListComponent {
 
-  constructor() { }
+  constructor(private router: Router) { }
 
-  ngOnInit() {
+  createTask() {
+    this.router.navigate(['/createTask']);
   }
 
 }
