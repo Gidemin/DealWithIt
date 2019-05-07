4a63f625842bbe44eecada9057b8366e933140d8
Gidemin <arabchik.viktor@mail.ru>
2019-05-07 20:24:42 +0300

    Implemented component for displaying task list

--- angular/src/app/task-list/task-list.component.ts
+++ angular/src/app/task-list/task-list.component.ts
@@ -1,5 +1,7 @@
 import { Component } from '@angular/core';
-import {  Router } from '@angular/router';
+import { Router } from '@angular/router';
+
+import { Task } from '../models/task';
 
 @Component({
   selector: 'app-task-list',
@@ -8,7 +10,13 @@ import {  Router } from '@angular/router';
 })
 export class TaskListComponent {
 
-  constructor(private router: Router) { }
+  public tasks: Task[] = [];
+
+  constructor(private router: Router) {
+      for (let i = 0; i < 20; i++) {
+         this.tasks.push(new Task('task_' + i, i + ' Test_data'));
+      }
+  }
 
   createTask() {
     this.router.navigate(['/createTask']);
