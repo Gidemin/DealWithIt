afa52f4496905774e944f3dda661b3757c4a422d
Gidemin <arabchik.viktor@mail.ru>
2019-05-19 20:44:51 +0300

    Implemented "create-task" component

--- angular/src/app/create-task/create-task.component.ts
+++ angular/src/app/create-task/create-task.component.ts
@@ -1,14 +1,42 @@
-import { Component } from '@angular/core';
+import {
+  Component,
+  ElementRef,
+  OnInit,
+  ViewChild
+} from '@angular/core';
 import { Router } from '@angular/router';
+import {
+  FormBuilder,
+  FormGroup
+} from '@angular/forms';
 
 @Component({
   selector: 'app-create-task',
   templateUrl: './create-task.component.html',
   styleUrls: ['./create-task.component.scss']
 })
-export class CreateTaskComponent {
+export class CreateTaskComponent implements OnInit {
 
-  constructor(private router: Router) { }
+  @ViewChild('text') textInput: ElementRef;
+  createTaskForm: FormGroup;
+
+  constructor(
+    private formBuilder: FormBuilder,
+    private router: Router
+  ) {
+    this.createTaskForm = this.formBuilder.group({
+      title: '',
+      text: ''
+    });
+  }
+
+  ngOnInit() {
+   this.textInput.nativeElement.focus();
+  }
+
+  createNote() {
+    console.log(this.createTaskForm.controls['title'].value, this.createTaskForm.controls['text'].value);
+  }
 
   backToTaskList() {
     this.router.navigate(['/taskList']);
