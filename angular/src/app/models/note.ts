1670c040d54980bf2613cea75457e157e991a65f
Gidemin <arabchik.viktor@mail.ru>
2019-06-24 04:23:46 +0300

    Added "date" property to the "note" Data model

--- angular/src/app/models/note.ts
+++ angular/src/app/models/note.ts
@@ -1,14 +1,19 @@
 export class Note {
     public title = '';
     public text = '';
+    public date: Date;
 
     constructor(
         text: string,
-        title?: string
+        title?: string,
+        date?: Date
     ) {
         this.text = text;
         if (title) {
             this.title = title;
+        }
+        if (date) {
+            this.date = date;
         }
     }
 
