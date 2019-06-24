21550e9798b1716912c6c7f9029271a8700ac32a
Gidemin <arabchik.viktor@mail.ru>
2019-06-24 03:23:16 +0300

    Added route to "note-edit"

--- angular/src/app/app-routing.module.ts
+++ angular/src/app/app-routing.module.ts
@@ -5,10 +5,12 @@ import {
 } from '@angular/router';
 
 import { CreateTaskComponent } from './create-task/create-task.component';
+import { NoteEditorComponent } from './note-editor/note-editor.component';
 import { TaskListComponent } from './task-list/task-list.component';
 
 const routes: Routes = [
   { path: 'createTask', component: CreateTaskComponent },
+  { path: 'editTask/:id', component: NoteEditorComponent },
   { path: 'taskList', component: TaskListComponent }
 ];
 
